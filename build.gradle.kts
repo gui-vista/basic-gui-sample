d97c8bed293918d96030cea4af99b9ba298cdff9
Nick Apperley <napperley@gmail.com>
2019-12-17 10:53:48 +1300

    Added Kotlin Multiplatform Gradle plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,14 @@
 group = "org.guivista"
 version = "0.1-SNAPSHOT"
+
+plugins {
+    kotlin("multiplatform") version "1.3.61"
+}
+
+kotlin {
+    linuxX64("linux") {
+        binaries {
+            executable("basic_gui")
+        }
+    }
+}
---
+++ src/linuxMain/kotlin/main.kt
@@ -0,0 +1,4 @@
+fun main() {
+    println("Starting Basic GUI...")
+    println("Exiting...")
+}
